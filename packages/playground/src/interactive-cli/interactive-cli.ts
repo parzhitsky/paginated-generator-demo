398876f8eb907373910d072de71ebe071388c21f
Dmytro Parzhytskyi <59528510+parzhitsky@users.noreply.github.com>
2024-01-03 22:56:47 +0200

    Fix IO types, make property readonly

--- packages/playground/src/interactive-cli/interactive-cli.ts
+++ packages/playground/src/interactive-cli/interactive-cli.ts
@@ -3,17 +3,22 @@ import readline from 'readline'
 export type Key = readline.Key
 
 export abstract class InteractiveCLI {
-  protected readlineInterface!: readline.Interface
+  protected readonly readlineInterface: readline.Interface
 
   constructor(
-    protected readonly input = process.stdin,
-    protected readonly output = process.stdout,
+    protected readonly input: NodeJS.ReadStream = process.stdin,
+    protected readonly output: NodeJS.WriteStream = process.stdout,
   ) {
-    this.input
-      .setRawMode(true)
-      .on('keypress', (input, key) => {
-        this.onKeyPress(key, input)
-      })
+    this.input.setRawMode(true).on('keypress', (input, key) => {
+      this.onKeyPress(key, input)
+    })
+
+    this.readlineInterface = readline.createInterface({
+      input: this.input,
+      output: this.output,
+    })
+
+    this.readlineInterface.pause()
   }
 
   protected afterStart(): void | PromiseLike<void> {}
@@ -21,10 +26,7 @@ export abstract class InteractiveCLI {
   protected abstract onKeyPress(key: Key, input: string): void
 
   async start(): Promise<void> {
-    this.readlineInterface = readline.createInterface({
-      input: this.input,
-      output: this.output,
-    })
+    this.readlineInterface.resume()
 
     await this.afterStart()
   }
